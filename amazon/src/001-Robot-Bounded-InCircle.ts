4ae0af101d14c87e5fc5fdfb2d1dbecd96502c51
Heang Ly <heanglypa@gmail.com>
2022-03-12 08:35:20 -0500

    robot bounded in circle

---
+++ amazon/src/001-Robot-Bounded-InCircle.ts
@@ -0,0 +1,26 @@
+const isRobotBounded = (instructions: string): boolean => {
+  // up, left, down, right
+  const directions = [
+    [0, 1],
+    [-1, 0],
+    [0, -1],
+    [1, 0]
+  ]
+
+  let i = 0
+  let x = 0
+  let y = 0
+
+  for (const dir of instructions) {
+    if (dir === 'L') {
+      i = (i + 1) % 4
+    } else if (dir === 'R') {
+      i = (i + 3) % 4
+    } else {
+      x += directions[i][0]
+      y += directions[i][1]
+    }
+  }
+
+  return (x === 0 && y === 0) || i !== 0
+}

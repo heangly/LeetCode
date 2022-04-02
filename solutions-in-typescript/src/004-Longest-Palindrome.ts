7ce7698d3327346c34ed63a9d8bcd39c1314cc87
Heang Ly <heanglypa@gmail.com>
2022-04-03 05:22:02 +0700

    init

---
+++ solutions-in-typescript/src/004-Longest-Palindrome.ts
@@ -0,0 +1,15 @@
+const longestPalindrome = (s: string): string => {
+  return ''
+}
+
+// Input: s = "babad"
+// Output: "bab"
+// Explanation: "aba" is also a valid answer.
+// Example 2:
+
+// Input: s = "cbbd"
+// Output: "bb"
+
+console.log(longestPalindrome('babad'))
+console.log(longestPalindrome('cbbd'))
+export {}

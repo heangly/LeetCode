52fefd4b73d8deb97acac62437b9a5fff6c00f42
Heang Ly <heanglypa@gmail.com>
2022-04-04 06:17:35 +0700

    longest substring palindrome

--- solutions-in-typescript/src/004-Longest-Palindrome.ts
+++ solutions-in-typescript/src/004-Longest-Palindrome.ts
@@ -1,5 +1,32 @@
 const longestPalindrome = (s: string): string => {
-  return ''
+  let max = ''
+
+  for (let i = 0; i < s.length; i++) {
+    const oddPalindrome = expand(i, i, s)
+    const eventPaldindrome = expand(i, i + 1, s)
+    let tempMax = ''
+
+    if (oddPalindrome.length > eventPaldindrome.length) {
+      tempMax = oddPalindrome
+    } else {
+      tempMax = eventPaldindrome
+    }
+
+    if (max.length < tempMax.length) {
+      max = tempMax
+    }
+  }
+
+  return max
+}
+
+const expand = (left: number, right: number, s: string): string => {
+  while (left >= 0 && right < s.length && s[left] === s[right]) {
+    left--
+    right++
+  }
+
+  return s.slice(left + 1, right)
 }
 
 // Input: s = "babad"

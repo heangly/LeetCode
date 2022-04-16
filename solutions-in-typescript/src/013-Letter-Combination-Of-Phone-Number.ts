22bc1344d37768358f005440b3af975c328e0de9
Heang Ly <heanglypa@gmail.com>
2022-04-17 06:50:59 +0700

    combination of phone number

---
+++ solutions-in-typescript/src/013-Letter-Combination-Of-Phone-Number.ts
@@ -0,0 +1,34 @@
+const letterCombinations = (digits: string): string[] => {
+  if (!digits.length) return []
+
+  const table = new Map<string, string[]>([
+    ['2', ['a', 'b', 'c']],
+    ['3', ['d', 'e', 'f']],
+    ['4', ['g', 'h', 'i']],
+    ['5', ['j', 'k', 'l']],
+    ['6', ['m', 'n', 'o']],
+    ['7', ['p', 'q', 'r', 's']],
+    ['8', ['t', 'u', 'v']],
+    ['9', ['w', 'x', 'y', 'z']]
+  ])
+
+  const ans: string[] = []
+
+  const permute = (str: string, idx: number) => {
+    if (idx === digits.length) {
+      ans.push(str)
+      return
+    }
+
+    for (const x of table.get(digits[idx])!) {
+      permute(str + x, idx + 1)
+    }
+  }
+
+  permute('', 0)
+  return ans
+}
+
+console.log(letterCombinations('23'))
+console.log(letterCombinations(''))
+console.log(letterCombinations('2'))

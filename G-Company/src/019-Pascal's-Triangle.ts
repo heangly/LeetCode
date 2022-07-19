21df1b7e9e8b9c3412a057a58a98118f0eb8246a
Heang Ly <heanglypa@gmail.com>
2022-07-19 09:03:34 -0400

    pascal

---
+++ G-Company/src/019-Pascal's-Triangle.ts
@@ -0,0 +1,17 @@
+// Time: O(n^2) | Space: O(1)
+const generate = (numRows: number): number[][] => {
+  const ans: number[][] = [[1]]
+
+  for (let i = 1; i < numRows; i++) {
+    const temp: number[] = [1]
+    for (let j = 1; j < ans[i - 1].length; j++) {
+      temp.push(ans[i - 1][j] + ans[i - 1][j - 1])
+    }
+    temp.push(1)
+    ans.push(temp)
+  }
+
+  return ans
+}
+
+console.log(generate(5))

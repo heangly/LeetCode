6b099610e56f63d95596b1dc9515bc2779e68dc0
Heang Ly <heanglypa@gmail.com>
2022-08-15 08:09:01 -0400

    unique path

---
+++ Dynamic-Programming/src/020-Unique-Path.ts
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/unique-paths/
+// Time: O(m + n) | Space: O(m + n)
+const uniquePaths = (m: number, n: number): number => {
+  const memo: { [key: string]: number } = {}
+
+  const dp = (r: number, c: number): number => {
+    const key = `r:${r}-c:${c}`
+    if (key in memo) return memo[key]
+
+    if (r >= m || c >= n) return 0
+    if (r === m - 1 && c === n - 1) return 1
+
+    memo[key] = dp(r + 1, c) + dp(r, c + 1)
+    return memo[key]
+  }
+
+  return dp(0, 0)
+}
+
+console.log(uniquePaths(3, 7))

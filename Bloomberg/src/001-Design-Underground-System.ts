7b489c2a9bb8074825ca6d5346b3c8c0139553aa
Heang Ly <heanglypa@gmail.com>
2022-09-02 08:21:36 -0400

    design underground system

--- Bloomberg/src/001-Design-Underground-System.ts
+++ Bloomberg/src/001-Design-Underground-System.ts
@@ -1,24 +1,47 @@
 // https://leetcode.com/problems/design-underground-system/
 
-interface Client {
-  id: string
-}
-
 class UndergroundSystem {
-  client: Record<string, string>
+  clientsCheckIn: Record<string, [string, number]>
+  stationTotalTime: Record<string, number>
+  stationTotalTimeLength: Record<string, number>
 
   constructor() {
-    this.client = {}
+    this.clientsCheckIn = {}
+    this.stationTotalTime = {}
+    this.stationTotalTimeLength = {}
   }
 
+  // Time: O(1)
   checkIn(id: number, stationName: string, t: number) {
-    // can only check in to 1 place at a time
+    if (!(id in this.clientsCheckIn)) {
+      this.clientsCheckIn[id] = [stationName, t]
+    } else {
+      this.clientsCheckIn[id].push(stationName, t)
+    }
   }
-  checkOut(id: number, stationName: string, t: number) {}
 
+  generateKey(startStation: string, endStation: string): string {
+    return `${startStation}-${endStation}`
+  }
+
+  // Time: O(1)
+  checkOut(id: number, stationName: string, t: number) {
+    const [startStationName, startTime] = this.clientsCheckIn[id]
+    const key = this.generateKey(startStationName, stationName)
+    const time = t - startTime
+
+    this.stationTotalTime[key] = (this.stationTotalTime[key] || 0) + time
+    this.stationTotalTimeLength[key] =
+      (this.stationTotalTimeLength[key] || 0) + 1
+
+    this.clientsCheckIn[id].pop()
+    this.clientsCheckIn[id].pop()
+  }
+
+  // Time: O(1)
   getAverageTime(startStation: string, endStation: string) {
-    // returns the average time it take to travel from statStation to endStation
-    // the average tie is computed from all the previous travelling times from start to end station directly
+    const key = this.generateKey(startStation, endStation)
+    return this.stationTotalTime[key] / this.stationTotalTimeLength[key]
   }
 }
 

c470f2ac5922d9bcc336ae2d074e0061732cf550
Heang Ly <heanglypa@gmail.com>
2022-09-06 09:33:24 -0400

    two city

---
+++ Bloomberg/002-Two-City-Scheduling.js
@@ -0,0 +1,22 @@
+const twoCitySchedCost = (costs) => {
+  costs.sort((a, b) => a[0] - a[1] - (b[0] - b[1]))
+  const mid = Math.floor(costs.length / 2)
+  let sum = 0
+
+  for (let i = 0; i < mid; i++) {
+    sum += costs[i][0] + costs[i + mid][1]
+  }
+
+  return sum
+}
+
+console.log(
+  twoCitySchedCost([
+    [259, 770],
+    [448, 54],
+    [926, 667],
+    [184, 139],
+    [840, 118],
+    [577, 469]
+  ])
+)

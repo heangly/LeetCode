186470db5b2fd942c18ba0e351ffabcaf731e4c8
Heang Ly <heanglypa@gmail.com>
2022-09-23 08:21:48 -0400

    max depth binary tree

---
+++ Recursion/src/008-Max-Depth-Binary-Tree.ts
@@ -0,0 +1,25 @@
+//https://leetcode.com/problems/maximum-depth-of-binary-tree/
+class TreeNode {
+  val: number
+  left: TreeNode | null
+  right: TreeNode | null
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val
+    this.left = left === undefined ? null : left
+    this.right = right === undefined ? null : right
+  }
+}
+
+// Time:O(n) | Space:O(d) where d is the max depth
+const maxDepth = (root: TreeNode | null): number => {
+  if (!root) return 0
+  return 1 + Math.max(maxDepth(root.left), maxDepth(root.right))
+}
+
+const tree = new TreeNode(3)
+tree.left = new TreeNode(9)
+tree.right = new TreeNode(20)
+tree.right.left = new TreeNode(15)
+tree.right.right = new TreeNode(7)
+
+console.log(maxDepth(tree))

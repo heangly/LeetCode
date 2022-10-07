0d31c52c420530e2d4710bf039c33e942316ee81
Heang Ly <heanglypa@gmail.com>
2022-10-07 08:28:41 -0400

    move 0

---
+++ Bloomberg/028-MOve-Zeroes.js
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/move-zeroes/
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+const moveZeroes = (nums) => {
+  let j = 0
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      nums[j] = nums[i]
+      j++
+    }
+  }
+
+  for (let i = j; i < nums.length; i++) {
+    nums[i] = 0
+  }
+
+  return nums
+}
+
+console.log(moveZeroes([0, 1, 0, 3, 12]))

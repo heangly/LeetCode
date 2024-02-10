9276ef092d05c84c9c21523583fabd9ba902d0a3
Heang Ly <heanglypa@gmail.com>
2024-02-10 07:54:38 -0500

    delete and earn

--- solutions-in-typescript/src/010-Delete-Earn.ts
+++ solutions-in-typescript/src/010-Delete-Earn.ts
@@ -2,21 +2,22 @@
 Pick any nums[i] and delete it to earn nums[i] points. Afterwards, you must delete every element equal to nums[i] - 1 and every element equal to nums[i] + 1.
  */
 
+// Time: O(N) | Space: O(N)
 function deleteAndEarn(nums: number[]): number {
-  const sortedArray = [...new Set(nums)].sort((a, b) => b - a)
-
-  const frequentMap: Record<string, number> = {}
+  const max = Math.max(...nums)
+  const dp = new Array(max + 1).fill(0)
 
   for (const num of nums) {
-    frequentMap[num] = (frequentMap[num] ?? 0) + num
+    dp[num] += num
   }
 
-  console.log(frequentMap)
+  for (let i = 2; i < dp.length; i++) {
+    dp[i] = Math.max(dp[i - 2] + dp[i], dp[i - 1])
+  }
 
-  // return topDown(0, sortedArray, frequentMap)
-  return 0
+  return dp.pop()!
 }
 
 // console.log(deleteAndEarn([3, 4, 2])) //6
 // console.log(deleteAndEarn([2, 2, 3, 3, 3, 4])) //9
-console.log(deleteAndEarn([1, 1, 1, 2, 4, 5, 5, 5, 6])) //18
+console.log(deleteAndEarn([2, 4, 5, 5, 5, 6])) //18

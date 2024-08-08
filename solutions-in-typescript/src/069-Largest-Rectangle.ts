126a445ea396795779f5be90698fffd81fa6f4d6
Heang Ly <heanglypa@gmail.com>
2024-08-08 13:38:21 -0400

    largest rectangle area

--- solutions-in-typescript/src/069-Largest-Rectangle.ts
+++ solutions-in-typescript/src/069-Largest-Rectangle.ts
@@ -1,16 +1,19 @@
+// Time: O(N) | Space: O(N)
 function largestRectangleArea(heights: number[]): number {
-  let min = heights[0]
-  let max = 0
+  heights.push(0)
+  const stack: { index: number; val: number }[] = []
+  let maxArea = 0
 
-  for (let i = 1; i < heights.length; i++) {
-    const curr = heights[i]
-    min = Math.min(min, curr)
-
-    const val = (i + 1) * min
-    max = Math.max(max, val)
+  for (let i = 0; i < heights.length; i++) {
+    const currHeight = heights[i]
+    while (stack.length && currHeight < stack[stack.length - 1].val) {
+      const topStack = stack.pop()!
+      const width = stack.length === 0 ? i : i - stack[stack.length - 1].index - 1
+      const height = topStack.val
+      maxArea = Math.max(maxArea, height * width)
+    }
+    stack.push({ index: i, val: heights[i] })
   }
 
-  return max
+  return maxArea
 }
-
-console.log(largestRectangleArea([2, 1, 5, 6, 2, 3]))

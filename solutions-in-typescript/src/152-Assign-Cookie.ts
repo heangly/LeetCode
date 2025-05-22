90bfd563e7f555f950d9839ca939abac577633da
Heang Ly <heanglypa@gmail.com>
2025-05-22 11:05:03 -0400

    clean up

--- solutions-in-typescript/src/152-Assign-Cookie.ts
+++ solutions-in-typescript/src/152-Assign-Cookie.ts
@@ -3,17 +3,15 @@
 function findContentChildren(g: number[], s: number[]): number {
   g.sort((a, b) => a - b)
   s.sort((a, b) => a - b)
-  let output = 0
   let i = 0
   let j = 0
 
   while (i < s.length && j < g.length) {
     if (s[i] >= g[j]) {
-      output++
       j++
     }
     i++
   }
 
-  return output
+  return j
 }

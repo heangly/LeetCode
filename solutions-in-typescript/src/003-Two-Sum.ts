3a6e1504ce638ea44b1437450b510f7bfd8e1512
Heang Ly <heanglypa@gmail.com>
2025-09-04 11:05:37 -0400

    two sum

---
+++ solutions-in-typescript/src/003-Two-Sum.ts
@@ -0,0 +1,16 @@
+// Time: O(N) | Space: O(N)
+function twoSum(nums: number[], target: number): number[] {
+  const table: Record<string, number> = {}
+
+  for (let i = 0; i < nums.length; i++) {
+    const searchingNum = target - nums[i]
+
+    if (searchingNum in table) {
+      return [table[searchingNum], i]
+    }
+
+    table[nums[i]] = i
+  }
+
+  return [-1, -1]
+}

19fc6d4c1a49bd061d53e2b77bc6c3eca83a7fcd
Heang Ly <heanglypa@gmail.com>
2025-09-29 23:17:55 -0400

    min stack

---
+++ solutions-in-typescript/src/014-Min-Stack.ts
@@ -0,0 +1,38 @@
+// Time: O(N)
+class MinStack {
+  private stack: { val: number; min: number }[] = []
+
+  constructor() {
+    this.stack = []
+  }
+
+  push(val: number): void {
+    if (!this.stack.length) {
+      this.stack.push({ val, min: val })
+    } else {
+      const min = Math.min(val, this.getMin())
+      this.stack.push({ val, min })
+    }
+  }
+
+  pop(): void {
+    this.stack.pop()
+  }
+
+  top(): number {
+    return this.stack[this.stack.length - 1].val
+  }
+
+  getMin(): number {
+    return this.stack[this.stack.length - 1].min
+  }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */

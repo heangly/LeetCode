9436c0d4790edd253c6024e85dc2d6195f2e277b
Heang Ly <heanglypa@gmail.com>
2026-06-09 11:26:57 -0400

    stock

---
+++ solutions-in-typescript/src/022-Best-Time-To-Buy-Sell-Stock.ts
@@ -0,0 +1,13 @@
+// Time: O(N)
+// Space: O(1)
+function maxProfit(prices: number[]): number {
+  let profit = 0
+
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i] > prices[i - 1]) {
+      profit += prices[i] - prices[i - 1]
+    }
+  }
+
+  return profit
+}
